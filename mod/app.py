18dfea6045230a749f8518a0205f0068849812d8
Y********* <y*********@gmail.com>
2024-11-14 11:37:50 +0200

    naujausia versija meniu brancho po merge su development

--- mod/app.py
+++ mod/app.py
@@ -1,1 +1,31 @@
-# Cia yra mainas.py
+from flask import Flask, render_template
+
+# Inicializuojame Flask aplikaciją
+app = Flask(__name__)
+
+# Sukuriame pagrindinį maršrutą, kuris grąžins pagrindinį puslapį
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/cargo')
+def prekes():
+    prekes = ["NOTV", "MTV", "TV"]
+    return render_template('prekes.html', prekes=prekes)
+
+@app.route('/login')
+def login():    
+    return render_template('login.html')
+
+@app.route('/reg')
+def reg():    
+    return render_template('reg.html')
+
+@app.route('/pirkejas')
+def pirkejas():
+    
+    return render_template('pirkejas.html')
+
+# Aplikacijos paleidimas
+if __name__ == '__main__':
+    app.run(debug=True)
