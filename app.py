140ffd4084399114eb7ebb7f5ff1754701044bf2
Y********* <y*********@gmail.com>
2024-11-14 09:49:33 +0200

    Veikia bendras meniu (Paulius)

--- app.py
+++ app.py
@@ -8,14 +8,18 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/prekes')
+@app.route('/cargo')
 def prekes():
     prekes = ["NOTV", "MTV", "TV"]
     return render_template('prekes.html', prekes=prekes)
 
-@app.route('/admin')
-def admin():    
-    return render_template('admin.html')
+@app.route('/login')
+def login():    
+    return render_template('login.html')
+
+@app.route('/reg')
+def reg():    
+    return render_template('reg.html')
 
 @app.route('/pirkejas')
 def pirkejas():
