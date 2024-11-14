29553495e3e073bb392a4dfd96965fc8c9befef0
Toasteris <aurisaluzas@gmail.com>
2024-11-14 13:34:47 +0200

    Galimybe adminui paziureti ir istrinti vartotojus

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-# Cia yra mainas.py
+from flask import render_template, redirect, url_for
+from flask import Flask
+from model.user_controller import user_blueprint
+from flask_login import current_user, login_required
+
+app = Flask(__name__)
+app.register_blueprint(user_blueprint)
+
+@app.route('/admin')
+@login_required
+def admin_dashboard():
+    if current_user.role != 'admin':
+        return redirect(url_for('profile'))
+    return render_template('admin/dashboard.html')
--- model/user_controller.py
+++ model/user_controller.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template
-from idp_classes import User, engine
+from model.idp_classes import User, engine
 from sqlalchemy.orm import sessionmaker
 
 Session = sessionmaker(bind=engine)
@@ -8,6 +8,6 @@ session = Session()
 user_blueprint = Blueprint('user', __name__)
 
 @user_blueprint.route('/admin/users')
-def perziureti_naudotojus():
+def view_users():
     users = session.query(User).all()
     return render_template('admin/view_users.html', users=users)
