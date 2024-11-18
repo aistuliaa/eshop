5cd7b0375b10cf7d85b85886021bf67404968852
aistuliaa <aiste.siudikaite@hotmail.com>
2024-11-18 09:19:08 +0200

    commitinu, kad nebutu konfliktu su Aurelijaus darbu

--- mod/model/prisijungimas_atsijungimas.py
+++ mod/model/prisijungimas_atsijungimas.py
@@ -11,7 +11,12 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from mod.model.idp_classes import User, engine
 
 app = Flask(__name__)
+<<<<<<< HEAD
 app.secret_key = 'dreamteam'  # nesu tikra ar mums butent sito reikia
+=======
+app.secret_key = 'dreamteam'
+# nesu tikra ar mums butent sito reikia
+>>>>>>> Funkciju_su_puslapiu_apjungimas
 
 Session = sessionmaker(bind=engine)
 session = Session()
@@ -23,6 +28,7 @@ def login():
             username = request.form.get('username')
             password = request.form.get('password')
 
+<<<<<<< HEAD
             try:
                 user = session.query(User).filter_by(username=username).first()
             except SQLAlchemyError as e:
@@ -74,11 +80,54 @@ def logout():
         flash('Atsijungimo klaida.')
     except Exception as e:
         flash(f"Unexpected error: {e}")  # Klaida kurios man nepavymo nuspeti
+=======
+        user = session.query(User).filter_by(username=username).first()
+        if not user:
+            flash('Vartotojas su tokiu prisijungimo vardu nerastas', 'error')
+            return redirect(url_for('login'))
+        
+        if user.is_blocked:
+            flash('Prisijungimas užblokuotas dėl per daug nesėkmingų bandymų', 'error')
+            return redirect(url_for('login'))
+        
+        if check_password_hash(user.password, password):
+            user.failed_logins = 0
+            session.commit()
+            
+            flask_session['user_id'] = user.id
+            flask_session['username'] = user.username
+            flash('Prisijungimas sėkmingas', 'success')
+            return redirect(url_for('home'))
+    # cia graziname i pagrindini puslapi, kurio vardas nezinau ar bus toks, todel cia ir pasizymiu
+        else:
+            user.failed_logins += 1  # cia jau padidinam skaiciu nesekmingu
+            if user.failed_logins >= 3:
+                user.is_blocked = True  # blokas po triju nesegeru
+            session.commit()
+            remaining_attempts = 3 - user.failed_logins
+            flash(f"Neteisingas slaptažodis. Likę bandymai: {remaining_attempts}", 'error')
+            return redirect(url_for('login'))
+        
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    flask_session.pop('user_id', None)
+    flask_session.pop('username', None)
+    flash('Sėkmingai atsijungėte.', 'success')
+>>>>>>> Funkciju_su_puslapiu_apjungimas
     return redirect(url_for('login'))
 
 @app.route('/home')
 def home():
+<<<<<<< HEAD
     return "Pagrindinis puslapis po prisijungimo."  # Placeholder pagrindiniam puslapiui
+=======
+    if 'user_id' in flask_session:
+        username = flask_session['username']
+        return render_template('index.html', username=username)
+    return render_template('index.html')
+>>>>>>> Funkciju_su_puslapiu_apjungimas
 
 if __name__ == '__main__':
     try:
